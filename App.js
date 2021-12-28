ad484a622e9a1903472c0111065bd0dc92203cfb
alexandresaints <alexandresaintss@gmail.com>
2021-12-28 02:26:39 -0300

    Bookshelf Finished

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {NavigationContainer} from '@react-navigation/native'
-import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Navbar from './src/Components/Navbar'
 import BookNavbar from "./src/Components/BookNavbar";
